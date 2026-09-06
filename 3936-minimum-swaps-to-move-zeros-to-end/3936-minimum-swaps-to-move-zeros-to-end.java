d5fee8c1cb62333817d92ce39d1204d0b8a3732d
Tushar Kushwaha <tushar7kushwaha@gmail.com>
2026-09-06 16:53:49 +0530

    Time: 1 ms (99.76%), Space: 46.6 MB (11.2%) - LeetHub

--- 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.java
+++ 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.java
@@ -4,25 +4,17 @@ class Solution {
         int right = nums.length-1;
         int left = 0;
         while(left<right){
-            while(nums[right] == 0&& right > left)right--;
-            while(nums[left] != 0 && left<right) left++;
+            while(nums[right]==0&&right>left) right--;
+            while(nums[left]!=0&&left<right) left++;
             if(nums[left]==0 && left<right){
                 int tmp = nums[left];
                 nums[left] = nums[right];
                 nums[right] = tmp;
                 swapCount++; 
+                left++;
+                right--;
             }
         }
-        // for(int left=0; left<nums.length; left++){
-        //     if(left>=right) break;
-        //     while(nums[right]==0 && right>left)right--;
-        //     if(nums[left]==0){
-        //         int tmp = nums[left];
-        //         nums[left] = nums[right];
-        //         nums[right] = tmp;
-        //         swapCount++;
-        //     }
-        // }
         return swapCount;
     }
 }
