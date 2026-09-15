a05cec1243d4223635f211cb4523a376ddafc8d2
Tushar Kushwaha <tushar7kushwaha@gmail.com>
2026-09-15 12:44:21 +0530

    Time: 13 ms (19.42%), Space: 46.5 MB (97.62%) - LeetHub

---
+++ Java/Easy/0136-single-number/0136-single-number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int singleNumber(int[] nums) {
+       Map<Integer, Integer> map = new HashMap<>();
+       for(int i : nums){
+        map.put(i, map.getOrDefault(i, 0)+1);
+       } 
+
+       for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+        if(entry.getValue()==1){
+            return entry.getKey();
+        }
+       }
+       return 0;
+    }
+}
